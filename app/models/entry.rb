c979fce7017c0e401424f6541a4f2482517b4f1a
kellenmay <kellenbmay@gmail.com>
2021-08-23 10:17:57 -0400

    scaffold in a user

--- app/models/entry.rb
+++ app/models/entry.rb
@@ -1,2 +1,4 @@
 class Entry < ApplicationRecord
+    belongs_to: User
+    belongs_to: Journal
 end
--- app/models/journal.rb
+++ app/models/journal.rb
@@ -1,2 +1,4 @@
 class Journal < ApplicationRecord
+    has_many: entries
+    has_many: users, through: entries
 end
